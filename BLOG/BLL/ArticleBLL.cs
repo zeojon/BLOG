02786122d414313db198c2f282fa44429bc6c79f
zeojon <superzhangjin@gmail.com>
2013-09-04 21:13:02 +0800

    g

---
+++ BLOG/BLL/ArticleBLL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ArticleBLL
+    {
+        private string _articleId;
+        private string _time;
+        private string _content;
+        private string _title;
+        private string _status;
+        private string _allowStatus;
+        private string _tagId;
+        private string _categoryId;
+
+        public IList<ArticleInfo> GetArticle()
+        { 
+        
+        }
+
+        public ArticleInfo GetArticle(int articleId)
+        { 
+        
+        }
+    }
+}
---
+++ BLOG/DAL/SqlHelper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    class SqlHelper
+    {
+        //从Web.config中读取数据库连接字符春
+        private String ConnStr = "server=.;database=blog;uid=sa;pwd=12345";
+
+        private SqlConnection conn = null;
+
+        /// <summary>
+        /// 将查询结果集填充到DataTable
+        /// </summary>
+        /// <param name="query">查询T-Sql</param>
+        /// <returns></returns>
+        public DataTable FillDataTable(String query)
+        {
+            DataTable dt = new DataTable();
+            using (conn = new SqlConnection(ConnStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = query;
+                SqlDataAdapter ada = new SqlDataAdapter();
+                ada.SelectCommand = cmd;
+                ada.Fill(dt);
+            }
+            return dt;
+        }
+
+        /// <summary>
+        /// 将查询结果集填充到DataSet
+        /// </summary>
+        /// <param name="query">查询T-Sql,可以是多个Select语句</param>
+        /// <returns></returns>
+        public DataSet FillDataSet(String query)
+        {
+            DataSet ds = new DataSet();
+            using (conn = new SqlConnection(ConnStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = query;
+                SqlDataAdapter ada = new SqlDataAdapter();
+                ada.SelectCommand = cmd;
+                ada.Fill(ds);
+            }
+            return ds;
+        }
+
+        /// <summary>
+        /// 执行insert、update、delete、truncate语句
+        /// </summary>
+        /// <param name="commandText">insert、update、delete、truncate语句</param>
+        public void ExecuteNonQuery(String commandText)
+        {
+            using (conn = new SqlConnection(ConnStr))
+            {
+                SqlTransaction tran = conn.BeginTransaction();
+                try
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+                    cmd.Transaction = tran;
+                    cmd.CommandText = commandText;
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    tran.Commit();
+                }
+                catch
+                {
+                    tran.Rollback();
+                }
+                finally
+                {
+                    tran.Dispose();
+                }
+            }
+        }
+
+    }
+}
